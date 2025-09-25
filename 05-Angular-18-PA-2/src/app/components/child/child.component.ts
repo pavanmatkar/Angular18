de8a25e1fb1421bb22fef2809306372311f35413
PavanMatkar <pavanmatakr123@gmail.com>
2025-09-25 10:14:19 +0530

    new project created

---
+++ 05-Angular-18-PA-2/src/app/components/child/child.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input() firstName: string = '';
+  @Input() lastName: string = '';
+  @Input() age: number = 0;
+}
